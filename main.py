5d4e766891e21b533134e70b3bf894a963d0c534
Bogdan <bogdanfesil@gmail.com>
2022-09-17 16:54:23 +0300

    Добавлено приветствие учителю

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-print('Калькулятор')
-a = int(input('Введите первое число: '))
-b = int(input('Введите второе число: '))
-print(f'{a} + {b} = {a + b}')
-print(f'{a} - {b} = {a - b}')
-print(f'{a} * {b} = {a * b}')
-print(f'{a} / {b} = {a / b}')
+print ('Hello teacher!')
